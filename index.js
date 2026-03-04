6b98de66a617998e90750f7a47048101c88e8a21
Adam Getbags <91522359+AdamGetbags@users.noreply.github.com>
2026-03-03 23:59:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,78 +1,114 @@
 const express = require('express')
+const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
+
 const app = express()
-const Note = require('./models/note')
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
 app.use(express.json())
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
 app.use(cors())
 app.use(express.static('dist'))
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
+  return String(Math.floor(Math.random() * 99999999))
 }
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
   })
 })
 
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+app.get('/info', (request, response) => {
+  Person.countDocuments({}).then(count => {
+    response.send(
+      `<div>
+        <p>Phonebook has info for ${count} people</p>
+        <p>${new Date()}</p>   
+      </div>`
+    )
+  })
 })
 
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
     })
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response, next) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
+  Person.find({}).then(persons => {
+    if (persons.map(person => person.name).includes(body.name)) {
+      return response.status(400).json({ 
+        error: `${body.name} exists already. DENIED.`
+      })
+    }
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
+    const person = new Person({
+      name: body.name,
+      number: body.number
     })
-    .catch(error => next(error))
+
+    person.save()
+      .then(savedPerson => {
+        response.json(savedPerson)
+      })
+      .catch(error => next(error))
+  })
 })
 
-app.put('/api/notes/:id', (request, response, next) => {
-  const { content, important } = request.body
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
-  Note.findById(request.params.id)
-    .then(note => {
-      if (!note) {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
         return response.status(404).end()
       }
 
-      note.content = content
-      note.important = important
+      person.name = name
+      person.number = number
 
-      return note.save().then((updatedNote) => {
-        response.json(updatedNote)
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
       })
     })
     .catch(error => next(error))
@@ -82,22 +118,20 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-// handler of requests with unknown endpoint
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
-
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
-  
+
   next(error)
 }
 
-// this has to be the last loaded middleware - all the routes should be registered before this!
+// this has to be the last loaded middleware, also all the routes should be registered before this!
 app.use(errorHandler)
 
 const PORT = process.env.PORT
